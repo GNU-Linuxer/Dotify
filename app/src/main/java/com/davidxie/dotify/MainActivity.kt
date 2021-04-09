ee51eed73ad8abefeb981335c37bcd5d597a8863
David Xie <xdkdavid@gmail.com>
2021-04-08 22:52:28 -0700

    viewbinding class not generated

--- app/src/main/java/com/davidxie/dotify/MainActivity.kt
+++ app/src/main/java/com/davidxie/dotify/MainActivity.kt
@@ -2,10 +2,31 @@ package com.davidxie.dotify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.davidxie.dotify.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(root) }
+
+
+    }
+
+//  Function that handeles when previous button is clicked
+    fun prevClicked(view: View) {
+        Toast.makeText(this, "Skipping to previous track", Toast.LENGTH_SHORT).show()
+    }
+
+    //  Function that handeles when next button is clicked
+    fun nextClicked(view: View) {
+        Toast.makeText(this, "Skipping to next track", Toast.LENGTH_SHORT).show()
     }
 }
