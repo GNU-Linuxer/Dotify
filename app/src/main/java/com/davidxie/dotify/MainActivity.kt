8ae9bd9fe5e5399f2c0b85c73f9089d218520517
David Xie <xdkdavid@gmail.com>
2021-04-09 10:48:21 -0700

    enable view binding

--- app/src/main/java/com/davidxie/dotify/MainActivity.kt
+++ app/src/main/java/com/davidxie/dotify/MainActivity.kt
@@ -3,7 +3,6 @@ package com.davidxie.dotify
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.Toast
 import com.davidxie.dotify.databinding.ActivityMainBinding
 
