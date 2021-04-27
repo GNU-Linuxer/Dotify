a7b92e96c174abb0824df93ae226752da7cc9311
Dongkai Xie <xdkdavid@gmail.com>
2021-04-27 14:10:00 -0700

    Using new way to pass intent bundle is not working

--- app/src/main/java/com/davidxie/dotify/SettingsActivity.kt
+++ app/src/main/java/com/davidxie/dotify/SettingsActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.davidxie.dotify.databinding.ActivitySettingsBinding
 import com.ericchee.songdataprovider.Song
 import com.ericchee.songdataprovider.SongDataProvider
 
@@ -24,39 +25,52 @@ fun navigateToSettingsActivity(context: Context, song: Song, playCount: Int) = w
 
 class SettingsActivity : AppCompatActivity() {
 
-    private val navController by lazy { findNavController(R.id.settingsFragmentHost) }
-
+    private val navController by lazy { findNavController(R.id.navHost) }
+    private lateinit var binding: ActivitySettingsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val launchIntent = intent
-        var songObj: Song? = launchIntent.extras?.getParcelable(SONG_KEY)
-        var playCount: Int? = launchIntent.extras?.getInt(PLAY_COUNT_KEY)
-
-        if (songObj == null) {
-            // if songObject is not passed in to this fragment, the first song will be used
-            val demoSong: Song = SongDataProvider.getAllSongs()[0]
-            songObj = demoSong
-            Toast.makeText(this@SettingsActivity,"Null Song Object detected", Toast.LENGTH_SHORT).show()
+        //DEBUG codes
+        intent.extras?.getParcelable<Song>(SONG_KEY)?.let{
+            Toast.makeText(this@SettingsActivity,it.title.toString(), Toast.LENGTH_SHORT).show()
         }
-        if (playCount == null) {
-            // if playCount is not passed in to this fragment, we set a playcount of -1
-            playCount = -1
-            Toast.makeText(this@SettingsActivity,"Null Player Count detected", Toast.LENGTH_SHORT).show()
+        intent.extras?.getInt(PLAY_COUNT_KEY)?.let{
+            Toast.makeText(this@SettingsActivity,it.toString(), Toast.LENGTH_SHORT).show()
         }
 
-        navController.setGraph(R.navigation.nav_graph, Bundle().apply {
-            putParcelable(SONG_KEY, songObj)
-            putInt(PLAY_COUNT_KEY, playCount)
-        })
+        binding = ActivitySettingsBinding.inflate(layoutInflater).apply { setContentView(root) }
+        with(binding){
+            // Programmatically set the nav graph
+            navController.setGraph(R.navigation.nav_graph, intent.extras)
+        }
 
-        setupActionBarWithNavController(navController)
+//        val launchIntent = intent
+//        var songObj: Song? = launchIntent.extras?.getParcelable(SONG_KEY)
+//        var playCount: Int? = launchIntent.extras?.getInt(PLAY_COUNT_KEY)
+//
+//        if (songObj == null) {
+//            // if songObject is not passed in to this fragment, the first song will be used
+//            songObj = SongDataProvider.getAllSongs()[0]
+//            Toast.makeText(this@SettingsActivity,"Null Song Object detected", Toast.LENGTH_SHORT).show()
+//        }
+//        if (playCount == null) {
+//            // if playCount is not passed in to this fragment, we set a playcount of -1
+//            playCount = -1
+//            Toast.makeText(this@SettingsActivity,"Null Player Count detected", Toast.LENGTH_SHORT).show()
+//        }
+//
+//        navController.setGraph(R.navigation.nav_graph, Bundle().apply {
+//            putParcelable(SONG_KEY, songObj)
+//            putInt(PLAY_COUNT_KEY, playCount)
+//        })
+
+        //setupActionBarWithNavController(navController)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        navController.navigateUp()
-        return true
-    }
+//    override fun onSupportNavigateUp(): Boolean {
+//        navController.navigateUp()
+//        return true
+//    }
 }
